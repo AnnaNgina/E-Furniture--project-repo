8d2d09602ba05c167fffc1ba764bee845788aa37
annastacia <annastaciangina96@gmail.com>
2022-06-26 23:20:03 +0300

    modifyind db.json file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let url="https://jsonplaceholder.typicode.com/users";
+let url="http://localhost:3000/rentals";
 fetch(url)
-.then(res => res.json())
+.then(res =>res.json())
 .then(data =>{
     
     let html='';
@@ -11,10 +11,10 @@ data.map(value =>{
 
     <div class="col-md-3">
     <div class="card" style="width:400px">
-        <img class="card-img-top" src="/images/capturer.png" alt="Card image" style="width:100%">
+        <img class="card-img-top" src="${value.imageurl}" style="width:100%">
         <div class="card-body">
-          <h4 class="card-title">${value.name}</h4>
-          <h3 class="card-description">${value.address.city}</h3>
+          <h4 class="card-title">${value.location}</h4>
+          <h3 class="card-description">${value.price}</h3>
           <p class="card-text">${value.phone}</p>
           <a href="#" class="btn btn-primary">See Profile</a>
         </div>
